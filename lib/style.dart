a77be94375e1a6a7d80614213f98c74ee7d0788e
SuhYuns <yan9hwa@gmail.com>
2023-11-13 00:19:12 +0900

    login

---
+++ lib/style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+var theme = ThemeData(
+  appBarTheme: AppBarTheme(
+      color: Colors.black,
+      actionsIconTheme: IconThemeData(
+        color: Colors.black,
+        size: 30,
+      )
+  ),
+
+  textTheme: TextTheme(
+    button: TextStyle(fontSize: 20),
+
+  ),
+
+
+);
