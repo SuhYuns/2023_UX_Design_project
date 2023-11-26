32dde2c754f9a38269373cf6eb19a6f138beba78
SuhYuns <yan9hwa@gmail.com>
2023-11-26 16:45:15 +0900

    all fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,173 +1,148 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import './style.dart';
+import 'dart:convert';
 
-final firestore = FirebaseFirestore.instance;
-final auth = FirebaseAuth.instance;
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  await initializeDateFormatting();
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: TableCalendarScreen()
+      )
   );
-
-  var result = await firestore.collection('product').get();
-  for (var doc in result.docs) {
-    print(doc['name']);
-  }
-
-
-  runApp(MaterialApp(
-    home: login(),
-    debugShowCheckedModeBanner: false,
-    theme: theme
-  ));
 }
 
-class login extends StatefulWidget {
-  login({super.key});
 
-  var loginState = Column(
-    children: [
-
-    ],
-  );
+class TableCalendarScreen extends StatefulWidget {
+  const TableCalendarScreen({Key? key}) : super(key: key);
 
   @override
-  State<login> createState() => _loginState();
+  State<TableCalendarScreen> createState() => _TableCalendarScreenState();
 }
 
-class _loginState extends State<login> {
-
-  var emailInput = TextEditingController();
-  var pwInput = TextEditingController();
-  var inputStyle = InputDecoration( border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),  );
-  // var buttonStyle =
-
-  loginProc(email, pw) async{
-
-    try {
-      await auth.signInWithEmailAndPassword(
-          email: email,
-          password: pw
-      );
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => index())
-      );
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  enrollProc(email, pw) async{
-
-    try {
-      var result = await auth.createUserWithEmailAndPassword(
-        email: email,
-        password: pw,
-      );
-      print(result.user);
-      showDialog(context: context, builder: (context){
-        return Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("회원가입 완료!"),
-              TextButton(
-                  onPressed: (){
-                    Navigator.pop(context);
-                  },
-                  child: Text("닫기")
-              )
-            ],
-          ),
-        );
-           // , addTwo : andTwo 하는 식으로 추가 가능
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
+class _TableCalendarScreenState extends State<TableCalendarScreen> {
+  var state = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("로그인 테스트"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(height: 50,),
-          Text("Email", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          TextField( controller: emailInput, decoration: inputStyle),
-          Container(height: 10,),
-          Text("PW", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          TextField( controller: pwInput, decoration: inputStyle),
-          Container(height: 30,),
-          Align(
-            alignment: Alignment.center,
-            child: TextButton(
-                style: ButtonStyle(),
-                onPressed: (){
-                  loginProc(emailInput.text, pwInput.text);
-                },
-                child: Text("로그인")
-            ),
-          ),
-            Align(
-            alignment: Alignment.center,
-            child: TextButton(
-                style: ButtonStyle(),
-                onPressed: (){
-                  enrollProc(emailInput.text, pwInput.text);
-                },
-                child: Text("회원가입")
-            )
-          ),
-
+        toolbarHeight: 80,
+        title: Text("trace", style: TextStyle(color: Colors.indigo)),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.list), onPressed: null),
+        actions: [
+          IconButton(icon: Icon(Icons.settings), onPressed: null),
         ],
-      )
+      ),
+
+      body: [home(), record()][state],
+
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.indigo,
+        onTap: (i){
+          setState(() {
+            state = i;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.dns), label: "홈"),
+          BottomNavigationBarItem(icon: Icon(Icons.leaderboard), label: "기록"),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.indigo,
+        child: Icon(Icons.add),
+        onPressed: (){
+          {};
+        },
+      ),
     );
   }
 }
 
 
-class index extends StatelessWidget {
-  index({super.key});
+class home extends StatefulWidget {
+  home({super.key});
 
+  @override
+  State<home> createState() => _homeState();
+}
 
-  initState() {
-    if(auth.currentUser?.uid == null){
-      print('로그인 안된 상태군요');
-    } else {
-      print('로그인 하셨네');
-    }
-  }
+class _homeState extends State<home> {
+
+  CalendarFormat format = CalendarFormat.week;
+
+  DateTime selectedDay = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              height: 50,
-            ),
-            Text("환영합니다! " + (auth.currentUser?.email).toString() + "님!"),
-            TextButton(
-                onPressed: () async {
-                  await auth.signOut();
-                },
-                child: Text("로그아웃")
-            ),
-          ],
-        ),
-      )
+    return TableCalendar(
+      // 캘린더 디자인 관련 부분
+      headerStyle: HeaderStyle(
+          formatButtonVisible: false,
+          titleCentered: true,
+          titleTextStyle: TextStyle(fontSize: 15)
+      ),
+      // calendarStyle: const CalendarStyle(
+      //
+      // ),
+
+      locale: 'ko_KR',
+      firstDay: DateTime.utc(2021, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: focusedDay,
+
+      calendarFormat: format,
+      onFormatChanged: (CalendarFormat format) {
+        setState(() {
+          this.format = format;
+        });
+      },
+      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+        // 선택된 날짜의 상태를 갱신합니다.
+        setState((){
+          this.selectedDay = selectedDay;
+          this.focusedDay = focusedDay;
+        });
+      },
+      selectedDayPredicate: (DateTime day) {
+        // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
+        return isSameDay(selectedDay, day);
+      },
+    );
+  }
+}
+
+
+
+class record extends StatefulWidget {
+  record({super.key});
+
+  @override
+  State<record> createState() => _recordState();
+}
+
+class _recordState extends State<record> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hello"),
     );
   }
 }
