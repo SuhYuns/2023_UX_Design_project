72cdf2e191555c95fb5c184b8c2dd5c54bd0adb3
SuhYuns <yan9hwa@gmail.com>
2023-11-30 19:37:46 +0900

    todo update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'dart:convert';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -90,41 +90,135 @@ class _homeState extends State<home> {
 
   DateTime focusedDay = DateTime.now();
 
+  var todoInput = TextEditingController();
+  var current_todo_items = [];
+
+  clear() async {
+    var storage = await SharedPreferences.getInstance();
+    storage.remove("todo_items");
+    print("삭제 완료!");
+  }
+
+  save_todo(item) async {
+    var storage = await SharedPreferences.getInstance();
+    var null_check = storage.getStringList('todo_items') ?? 'none';
+    var date = selectedDay.year.toString() + selectedDay.month.toString() + selectedDay.day.toString();
+
+    if (null_check == 'none') {
+      // 기존 요소 없을 시 초기화
+      storage.setStringList("todo_items", []);
+    }
+
+    var item_list = storage.getStringList("todo_items");
+    item_list?.add(jsonEncode({ 'date' : date, "item" :  item}));
+    storage.setStringList('todo_items', item_list!);
+    print("저장 완료!");
+
+  }
+
+  view_todo() async {
+    var storage = await SharedPreferences.getInstance();
+    var null_check = storage.getStringList('todo_items') ?? 'none';
+    var date = selectedDay.year.toString() + selectedDay.month.toString() + selectedDay.day.toString();
+
+    if (null_check == 'none') {
+      // 기존 요소 없을 시 초기화
+      storage.setStringList("todo_items", []);
+    }
+
+    setState(() {
+      current_todo_items = [];
+      var item_list = storage.getStringList("todo_items");
+      item_list?.forEach((i) {
+        print(jsonDecode(i)['item']);
+        print(jsonDecode(i)['date']);
+        if (jsonDecode(i)['date'] == date) {
+          current_todo_items.add(jsonDecode(i)['item']);
+        }
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    view_todo();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      // 캘린더 디자인 관련 부분
-      headerStyle: HeaderStyle(
-          formatButtonVisible: false,
-          titleCentered: true,
-          titleTextStyle: TextStyle(fontSize: 15)
-      ),
-      // calendarStyle: const CalendarStyle(
-      //
-      // ),
+    return Column(
+      children: [
+        TableCalendar(
+          // 캘린더 디자인 관련 부분
+          headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextStyle: TextStyle(fontSize: 15)
+          ),
+          // calendarStyle: const CalendarStyle(
+          //
+          // ),
 
-      locale: 'ko_KR',
-      firstDay: DateTime.utc(2021, 10, 16),
-      lastDay: DateTime.utc(2030, 3, 14),
-      focusedDay: focusedDay,
+          locale: 'ko_KR',
+          firstDay: DateTime.utc(2021, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: focusedDay,
 
-      calendarFormat: format,
-      onFormatChanged: (CalendarFormat format) {
-        setState(() {
-          this.format = format;
-        });
-      },
-      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-        // 선택된 날짜의 상태를 갱신합니다.
-        setState((){
-          this.selectedDay = selectedDay;
-          this.focusedDay = focusedDay;
-        });
-      },
-      selectedDayPredicate: (DateTime day) {
-        // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
-        return isSameDay(selectedDay, day);
-      },
+          calendarFormat: format,
+          onFormatChanged: (CalendarFormat format) {
+            setState(() {
+              this.format = format;
+            });
+          },
+          onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+            // 선택된 날짜의 상태를 갱신합니다.
+            setState((){
+              this.selectedDay = selectedDay;
+              this.focusedDay = focusedDay;
+              view_todo();
+            });
+
+          },
+          selectedDayPredicate: (DateTime day) {
+            // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
+            return isSameDay(selectedDay, day);
+          },
+        ),
+
+        ListView.builder(shrinkWrap: true,
+              itemCount: current_todo_items.length,
+              itemBuilder: (c, i) {
+                return ListTile(
+                  leading: Icon(Icons.check_box_outline_blank),
+                  title: Text(current_todo_items[i].toString()),
+                );
+          }
+          ),
+        Container(
+          height: 50,
+        ),
+        TextField(
+          controller: todoInput,
+          decoration: InputDecoration(
+            labelText: "todo 추가",
+              filled: true,
+              fillColor: Colors.indigo.shade100,
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide.none, // 테두리 선을 없애준다
+              )
+          ),
+
+        ),
+        TextButton(
+          child: Text("추가"),
+          onPressed: () {
+            save_todo(todoInput.text);
+            view_todo();
+          },
+        ),
+      ],
     );
   }
 }
