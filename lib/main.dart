64516f53ea79e7346571197c8b6b9787989d9438
SuhYuns <yan9hwa@gmail.com>
2023-12-03 02:32:57 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'dart:convert';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
@@ -15,7 +14,6 @@ void main() async {
       )
   );
 }
-
 
 class Main extends StatefulWidget {
   const Main({Key? key}) : super(key: key);
@@ -36,9 +34,42 @@ class _Main extends State<Main> {
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.list), onPressed: null),
         actions: [
-          IconButton(icon: Icon(Icons.settings), onPressed: null),
+
+          Text("비회원입니다"),
+          PopupMenuButton(
+            // add icon, by default "3 dot" icon
+
+            icon: Icon(Icons.list, color: Colors.black),
+              itemBuilder: (context){
+                return [
+                  PopupMenuItem<int>(
+                    value: 1,
+                    child: Text("로그아웃"),
+                  ),
+
+                  PopupMenuItem<int>(
+                    value: 2,
+                    child: Text("자료 백업/로드"),
+                  ),
+
+                  PopupMenuItem<int>(
+                    value: 3,
+                    child: Text("문의하기"),
+                  ),
+
+                ];
+              },
+              onSelected:(value){
+                if(value == 0){
+                  print("My account menu is selected.");
+                }else if(value == 1){
+                  print("Settings menu is selected.");
+                }else if(value == 2){
+                  print("Logout menu is selected.");
+                }
+              }
+          ),
         ],
       ),
 
@@ -59,40 +90,71 @@ class _Main extends State<Main> {
         ],
       ),
 
-      // floatingActionButton: FloatingActionButton(
-      //   backgroundColor: Colors.indigo,
-      //   child: Icon(Icons.add),
-      //   onPressed: (){
-      //     showDialog(context: context, barrierDismissible: true, builder: (context) {
-      //       return addBlock();
-      //     });
-      //   },
-      // ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.indigo,
+        child: Icon(Icons.add),
+        onPressed: (){
+          showDialog(context: context, barrierDismissible: true, builder: (context) {
+            return addLocation();
+          });
+        },
+      ),
     );
   }
 }
 
-class addBlock extends StatelessWidget {
+
+
+class addBlock extends StatefulWidget {
   addBlock({super.key, this.selectedDay, this.view_todo});
   final selectedDay;
   final view_todo;
+
+  @override
+  State<addBlock> createState() => _addBlockState();
+}
+
+class _addBlockState extends State<addBlock> {
   var todoInput = TextEditingController();
+  var currentValue = '1 menu';
+  var loc_list;
+
+  var _valueList = ["미설정"];
+  var _selectedValue = "미설정";
 
   save_todo(item) async {
     var storage = await SharedPreferences.getInstance();
     var null_check = storage.getStringList('todo_items') ?? 'none';
-    var date = selectedDay.year.toString() + selectedDay.month.toString() + selectedDay.day.toString();
-    var location = "건국대학교";
+    var date = widget.selectedDay.year.toString() +
+        widget.selectedDay.month.toString() + widget.selectedDay.day.toString();
+    var location = _selectedValue;
 
     if (null_check == 'none') {
-      // 기존 요소 없을 시 초기화
       storage.setStringList("todo_items", []);
     }
 
     var item_list = storage.getStringList("todo_items");
-    item_list?.add(jsonEncode({ 'date' : date, "item" :  item, "done" : false, "location" : location}));
+    item_list?.add(jsonEncode(
+        { 'date': date, "item": item, "done": false, "location": location}));
     storage.setStringList('todo_items', item_list!);
     print("저장 완료!");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _asyncMethod();
+  }
+
+  _asyncMethod() async {
+    var storage = await SharedPreferences.getInstance();
+    var null_check = storage.getStringList('locations') ?? 'none';
+
+    if (null_check == 'none') {
+      storage.setStringList("locations", ["미설정"]);
+    }
+
+    _valueList = storage.getStringList("locations")!.toList();
 
   }
 
@@ -111,9 +173,12 @@ class addBlock extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text(
-              selectedDay.year.toString() + " " + selectedDay.month.toString() + " " +  selectedDay.day.toString(),
-              style: TextStyle(fontSize: 15),
+              widget.selectedDay.year.toString() + " " +
+                  widget.selectedDay.month.toString() + " " +
+                  widget.selectedDay.day.toString(),
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
             ),
+            _menuBtn(),
             TextField(
               controller: todoInput,
               decoration: InputDecoration(
@@ -130,7 +195,7 @@ class addBlock extends StatelessWidget {
               child: Text("추가"),
               onPressed: () {
                 save_todo(todoInput.text);
-                view_todo();
+                widget.view_todo();
                 Navigator.pop(context);
               },
             ),
@@ -139,7 +204,108 @@ class addBlock extends StatelessWidget {
       ),
     );
   }
+
+  Widget _menuBtn() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+      margin: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Colors.indigo
+        ),
+        color: Color.fromARGB(245, 245, 245, 245)
+      ),
+      child: DropdownButton(
+        style: TextStyle(backgroundColor: Color.fromARGB(245, 245, 245, 245)),
+        value: _selectedValue,
+        // for(String str in loc_list!)
+        //   DropdownMenuItem(value: str.toString(), child: Text(str)),
+        items: _valueList.map(
+            (value) {
+              return DropdownMenuItem(
+                value: value,
+                  child: Text(value)
+              );
+            }
+        ).toList(),
+        onChanged: (String? value) {
+          setState(() {
+            _selectedValue = value!;
+          });
+        },
+      ),
+    );
+  }
 }
+
+
+
+
+class addLocation extends StatelessWidget {
+  addLocation({super.key});
+
+  var LocInput = TextEditingController();
+
+  addLoc() async {
+    var storage = await SharedPreferences.getInstance();
+    var null_check = storage.getStringList('locations') ?? 'none';
+
+    if (null_check == 'none') {
+      storage.setStringList("locations", ["미설정"]);
+    }
+
+    var loc_list = storage.getStringList("locations");
+    loc_list?.add(LocInput.text);
+    print(loc_list);
+    storage.setStringList('locations', loc_list!);
+    print("저장 완료!");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.grey.shade100,
+      insetPadding: const EdgeInsets.symmetric(
+        horizontal: 50,
+        vertical: 100,
+      ),
+      child: SizedBox(
+        width: 100,
+        height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "장소 추가하기",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            TextField(
+              controller: LocInput,
+              decoration: InputDecoration(
+                  labelText: "장소를 추가해 주세요",
+                  filled: true,
+                  fillColor: Colors.indigo.shade100,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none, // 테두리 선을 없애준다
+                  )
+              ),
+
+            ),
+            TextButton(
+              child: Text("추가"),
+              onPressed: () {
+                addLoc();
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );;
+  }
+}
+
 
 
 
@@ -165,6 +331,10 @@ class _homeState extends State<home> {
   var todoInput = TextEditingController();
   var current_todo_items = [];
 
+  var items_count = 0;
+  var items_done = 0;
+  var percentage = 0;
+
   clear() async {
     var storage = await SharedPreferences.getInstance();
     storage.remove("todo_items");
@@ -186,17 +356,26 @@ class _homeState extends State<home> {
       current_todo_items = [];
       var item_list = storage.getStringList("todo_items");
       var counting = 0;
+      items_count = 0;
+      items_done = 0;
       item_list?.forEach((i) {
         if (jsonDecode(i)['date'] == date) {
+          items_count += 1;
           var test = jsonDecode(i);
           test["number"] = counting;
           current_todo_items.add(test);
+          if (test["done"] == true) {
+            items_done += 1;
+          }
         }
         counting += 1;
       });
+
+      percentage = ((items_done / items_count) * 100) as int;
+      if (items_count == 0) percentage = 0;
     });
   }
-  //
+
   change_state(target) async {
     var storage = await SharedPreferences.getInstance();
     var item_list = storage.getStringList("todo_items");
@@ -229,7 +408,6 @@ class _homeState extends State<home> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     view_todo();
   }
@@ -275,6 +453,18 @@ class _homeState extends State<home> {
                 // selectedDay 와 동일한 날짜의 모양을 바꿔줍니다.
                 return isSameDay(selectedDay, day);
               },
+            ),
+
+            Row(
+              children: [
+                Text("    전체 목록 : " + items_count.toString()),
+                Text("   완료한 목록 : " + items_done.toString() + "(" + percentage.toString() + "%)"),
+              ],
+
+            ),
+
+            Container(
+              height: 20,
             ),
 
             ListView.builder(shrinkWrap: true,
